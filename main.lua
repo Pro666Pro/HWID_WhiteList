6ece0e74b6fa722626863e1ab681b3416a931166
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-22 00:49:26 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -110,6 +110,7 @@ return {
   "d29c018a-73fe-472b-bdab-90c566fbd8a9", 
   "de70b562-c120-4e6a-a0f3-05de696de6f7",
   "4E552F8F-B118-4525-AE5E-89F49D7FFC2F",
+  "C60B3E1D-FB42-4248-92FD-F8A6679B8963",
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
