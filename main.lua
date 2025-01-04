5719ffe08b2d4be85f63548100a8152281cc208b
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-01-04 16:01:37 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -133,6 +133,7 @@ return {
   "a7e83647-4597-4eb6-9252-f487cd3fd410",
   "0601f168-7f93-4510-86d5-75d5785af7e4",
   "07c069a9-7398-458f-9c5d-3953a2b57a6b",
+  "CA755DAC-1C52-4C4D-9A0A-2DC50F3D0A86",
   "skibidi" -- my hwid
 }
 -- what are you doing here bro
