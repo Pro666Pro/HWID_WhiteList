ec6e1498cb54718aa057e8d6a7e39ab44dafd12e
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-01-13 00:08:50 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -138,6 +138,7 @@ return {
   "02008d7e-eb77-450d-b504-be321ac32694",
   "1845493c-b414-4551-9683-d7a7ba166825",
   "70A872CD-FEA9-4C4E-B80E-D4E712C18390",
+  "c368116b-8224-4d2d-a5cd-d25f7716dc2c",
   "skibidi" -- my hwid
 }
 -- what are you doing here bro
